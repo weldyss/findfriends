c4a61d8f12951161500b95e38dc0c92c675e6798
Weldys Santos <weldyss@gmail.com>
2012-03-12 14:22:39 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findfriends::Application.config.secret_token = 'bdef12cd3259e99786804160f72e35c1a749922f1a5ad5ffdf5dd5743a60c235b7407fff714d9e61ede6d95406107f1b58447223680184e82548429e070a4379'
